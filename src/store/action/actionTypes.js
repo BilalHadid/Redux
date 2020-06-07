46ccb93c520a5f9ef18679308a001ad1d9f4f50d
bilal hadid <hadid0648@gmail.com>
2020-06-08 01:40:27 +0500

    Complete

---
+++ src/store/action/actionTypes.js
@@ -0,0 +1,2 @@
+export const increse_count = 'increse_count'
+export const decrement_count = 'decrement_count'
--- src/store/action/countAction.js
+++ src/store/action/countAction.js
@@ -1,2 +1,4 @@
-export const increase_value = (value) => ({type:'increse_count', Payload: value})
-export const decrease_value = (value) => ({type: 'decrement_count',Payload:value})
+import {increse_count,decrement_count} from './actionTypes.js'
+
+export const increase_value = (value) => ({type: increse_count, Payload: value})
+export const decrease_value = (value) => ({type: decrement_count,Payload:value})
